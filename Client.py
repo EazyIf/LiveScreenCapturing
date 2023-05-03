c719bc6dd7e26a52d99de1be1d13cc77d48f9f08
EazyIf <93876789+EazyIf@users.noreply.github.com>
2023-05-03 23:01:21 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,6 +2,11 @@ import socket,cv2, pickle,struct
 from mss import mss
 from PIL import Image
 import numpy as np
+import ctypes
+
+user32 = ctypes.windll.user32
+win_x, win_y = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)] 
+win_cnt_x, win_cnt_y = [user32.GetSystemMetrics(0)/2, user32.GetSystemMetrics(1)/2]
 
 # create socket
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -9,6 +14,9 @@ host_ip = '192.168.1.126' # paste your server ip address here
 port = 9999
 client_socket.connect((host_ip,port)) # a tuple
 data = b""
+packed_data = struct.pack("ii", win_x, win_y)
+client_socket.sendall(packed_data)
+
 payload_size = struct.calcsize("Q")
 while True:
 	while len(data) < payload_size:
@@ -24,6 +32,8 @@ while True:
 	frame_data = data[:msg_size]
 	data  = data[msg_size:]
 	frame = pickle.loads(frame_data)
+	cv2.namedWindow("RECEIVING VIDEO", cv2.WINDOW_NORMAL)
+	cv2.setWindowProperty("RECEIVING VIDEO", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 	cv2.imshow("RECEIVING VIDEO",frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key  == ord('q'):

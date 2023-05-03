1a8e9f6a2b8a7c2b9ff439a218016a94f6e3668c
EazyIf <93876789+EazyIf@users.noreply.github.com>
2023-05-03 23:01:49 +0300

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,7 +1,7 @@
 import socket, cv2, pickle, struct, imutils
 import numpy as np
 from mss import mss
-
+import keyboard
 # Socket Create
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
@@ -22,24 +22,23 @@ while True:
     client_socket,addr = server_socket.accept()
     print('GOT CONNECTION FROM:',addr)
     if client_socket:
+        data = client_socket.recv(8)
+        win_x, win_y = struct.unpack("ii",data)
         # Initialize the screen capture object
         monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
         sct = mss()
         
-
         while True:
             sct_img = sct.grab(monitor)
             frame = np.array(sct_img)
             frame = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
             
-            # frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
-            
-            frame = imutils.resize(frame, width=1280)
+            frame = imutils.resize(frame, win_x)
             a = pickle.dumps(frame)
             message = struct.pack("Q",len(a))+a
             client_socket.sendall(message)
 
-            cv2.imshow('TRANSMITTING SCREEN',frame)
+            # cv2.imshow('TRANSMITTING SCREEN',frame)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 break

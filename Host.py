87524da84adbc7895cd67871b47a2fa3c0ed7bbd
EazyIf <93876789+EazyIf@users.noreply.github.com>
2023-05-08 18:12:12 +0300

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,30 +1,30 @@
-import socket, cv2, pickle, struct, imutils
+import socket
+import cv2
+import pickle
+import struct
+import imutils
 import numpy as np
 from mss import mss
 import keyboard
-# Socket Create
+
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
 host_name  = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
 print('HOST IP:',host_ip)
 port = 9999
 socket_address = (host_ip,port)
 
-# Socket Bind
 server_socket.bind(socket_address)
-
-# Socket Listen
 server_socket.listen(5)
 print("LISTENING AT:",socket_address)
 
-# Socket Accept
 while True:
     client_socket,addr = server_socket.accept()
     print('GOT CONNECTION FROM:',addr)
     if client_socket:
         data = client_socket.recv(8)
         win_x, win_y = struct.unpack("ii",data)
-        # Initialize the screen capture object
         monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
         sct = mss()
         

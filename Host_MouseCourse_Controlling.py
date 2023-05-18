900365b0762ce96240d835ce5c876baa46ebdc31
EazyIf <93876789+EazyIf@users.noreply.github.com>
2023-05-18 19:10:02 +0300

    Update Host_MouseCourse_Controlling.py

--- Host_MouseCourse_Controlling.py
+++ Host_MouseCourse_Controlling.py
@@ -15,65 +15,27 @@ client_socket, client_address = server_socket.accept()
 screenW, screenH = pyautogui.size()
 midX = screenW // 2
 midY = screenH // 2
+
 mouse = Controller()
 mouse.position = (midX,midY)
 
-def get_button_from_string(button_str):
-    if button_str == 'Button.left':
-        return Button.left
-    elif button_str == 'Button.right':
-        return Button.right
-    elif button_str == 'Button.middle':
-        return Button.middle
-    else:
-        return Button.unknown
-
-try:
-    while True:
+while True:
+    try:
         data = client_socket.recv(1024).decode()
-        if not data:
-            break
-        events = data.split('\n')
-        for event in events:
-            event_parts = event.split()
-            if len(event_parts) < 2:
-                continue
-            
-            event_type = event_parts[0]
+        if ',' in data:
+            x, y = map(int, data.split(","))
+            mouse.position = (x, y)
 
-            if event_type == 'MOVE':
-                if len(event_parts) < 3:
-                    continue
-                try:
-                    x, y = int(event_parts[1]), int(event_parts[2])
-                    mouse.position = (x, y)
-                except ValueError:
-                    continue
-            
-            elif event_type == 'PRESS' or event_type == 'RELEASE':
-                if len(event_parts) < 4:
-                    continue
-                button_str = event_parts[1]
-                button = get_button_from_string(button_str)
-                try:
-                    x, y = int(event_parts[2]), int(event_parts[3])
-                    if event_type == 'PRESS':
-                        mouse.position = (x, y)
-                        if button == mouse.scroll:
-                            if len(event_parts) < 5:
-                                continue
-                            scroll_amount = int(event_parts[4])
-                            mouse.scroll(100, scroll_amount)
-                        else:
-                            mouse.press(button)
-                    else:
-                        mouse.position = (x, y)
-                        if button != mouse.scroll:
-                            mouse.release(button)
-                except ValueError:
-                    continue
-except KeyboardInterrupt:
-    pass
+        elif 'scroll' in data:
+            mouse.scroll(0, int(data.split('.')[1]))
+        else:
+            buttonn = getattr(Button,f'{data.split(".")[2]}')
+            action = getattr(mouse,f'{data.split(".")[0]}')
+            action(buttonn)
 
-client_socket.close()
-server_socket.close()
+    except ValueError:
+        pass
+        # print(ValueError, " too many values to unpack (expected 2)")
+    except AttributeError:
+        pass
+        # print(AttributeError, " 'Button' object has no attribute '",data.split(".")[0],"'")
